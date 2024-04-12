a992f8eb13a50aa1f94f590b998ca1f156a985d4
louisexpat <louis@expat.com>
2024-04-12 20:08:50 +0400

    add vercel analystics

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,14 @@
 export default defineAppConfig({
   ui: {
     primary: 'blue',
-  }
+  },
+  vercelAnalytics: {
+    mode: "auto",
+    debug: true,
+    beforeSend: (event) => {
+      if (event.url.includes("/private")) return null;
+
+      return event;
+    },
+  },
 })
